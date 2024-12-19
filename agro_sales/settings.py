e223c1de94b28bae3a1d78a5a2e881b7839657ce
allahatim <hatime.alla@gmail.com>
2024-12-19 21:02:32 +0100

    fix cors issues

--- agro_sales/settings.py
+++ agro_sales/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,13 +50,9 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
-CORS_ALLOW_ALL_ORIGINS = True 
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000', 
-    'https://yourfrontenddomain.com',
-]
 ROOT_URLCONF = 'agro_sales.urls'
 
 TEMPLATES = [
@@ -138,3 +134,9 @@ import os
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+CORS_ALLOW_ALL_ORIGINS = True 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',  
+    'https://yourfrontenddomain.com',
+]
