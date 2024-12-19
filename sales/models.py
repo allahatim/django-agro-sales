ab2af37ea42789fec76ae0851074e1eb2a587672
allahatim <hatime.alla@gmail.com>
2024-12-19 20:34:02 +0100

    back end code

--- sales/models.py
+++ sales/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Product(models.Model):
     image = models.ImageField(upload_to='product_images/', blank=True, null=True)
     name = models.CharField(max_length=100)
-    description = models.CharField(max_length=500)
+    description = models.TextField(null=True, blank=True)
     stock = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     def __str__(self):
